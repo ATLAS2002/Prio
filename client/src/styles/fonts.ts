0a5f8aa50bacb9724e026d48d0256cc77e572911
ATLAS2002 <siddharth211102@gmail.com>
2023-09-20 18:28:07 +0530

    Home page background and titel added  and created a demo server

---
+++ client/src/styles/fonts.ts
@@ -0,0 +1,8 @@
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({ 
+    weight: '700',
+    subsets: ['latin']
+})
+
+export { poppins }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,27 @@
-import { Elysia } from "elysia";
+import { Elysia, ws } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(8000);
+const app = new Elysia()
+    .use(ws())
+    .ws('/ws', {
+        message: (ws, message) => {
+            ws.send({
+                message,
+                time: Date.now()
+            })
+        },
+        open: (ws) => {
+            ws.send('Websocket connection established')
+        },
+        close: (ws) => {
+            ws.send('Connection closed')
+        }
+    })
+    .get('/', () => 'home')
+    .get('/ping', () => 'pong1')
+    .listen(8000)
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+    `🦊 Elysia is running at ${
+        'http://' + app.server?.hostname + ':' + app.server?.port
+    }`
+)
